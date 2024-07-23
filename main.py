bb6c21cd22e9b5c3d6293d4a93905454275d6a70
farisnf16 <fariz.crass@gmail.com>
2024-07-23 12:22:11 +0700

    hck

---
+++ main.py
@@ -0,0 +1,45 @@
+#import packet
+from fastapi import FastAPI, Request, header, HTTPException
+import pandas as pd
+
+#buat dataframe baru
+df = pd.DataFrame()
+
+df['UserName'] = ['Undertaker', 'Rey', 'edge']
+df['Location'] = ['texas','maxico','colombia']
+
+#pswd api_key
+API_KEY = "testingapitokenkey1234"
+
+#BUAT OBJEK
+app = FastAPI()
+
+
+# membuat function * url (endpoint)
+
+# endpoint untuk retridve all data
+# http://www.domain.com
+@app.get("/")
+def handlerdata(request: Request):
+    # get request headers
+    headers = request.headers
+
+    return {
+        "masssege": "this is fastapi data",
+        "headers": headers
+    }
+
+# endpoint secret
+@app.get("/secret")
+def handerSecret(api_key: str = header(None)):
+    #cek api_key
+    if api_key != API_KEY:
+        return {
+            "error":"anda tidak lolos"
+        }
+    
+# endpoint get all data from dataframe
+@app.get("/data")
+def handlerDf():
+    return df.to_dict(orient="record")
+
